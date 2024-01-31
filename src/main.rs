a2a21c844cb95d496631b98eae9790578af44c3c
psznm <psznm@seznam.cz>
2024-01-31 21:42:43 +0100

    Some error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, error::Error, fs, os::unix, path::PathBuf};
+use std::{collections::HashMap, fs, os::unix, path::PathBuf};
 
 use clap::Parser;
 use log::{debug, info, trace};
@@ -34,11 +34,7 @@ impl ProjectDirectory {
         }
     }
 
-    fn add_link(
-        &mut self,
-        link: &String,
-        destination_relative: &String,
-    ) -> Result<(), Box<dyn Error>> {
+    fn add_link(&mut self, link: &String, destination_relative: &String) {
         let destination_abs = self.path.join(destination_relative);
         self.links.push(Link {
             link: link.into(),
@@ -49,8 +45,6 @@ impl ProjectDirectory {
             link: PathBuf::from("node_modules").join(link),
             destination_abs,
         });
-
-        Ok(())
     }
 
     fn get_absolute_links(&self) -> impl Iterator<Item = (PathBuf, PathBuf)> + '_ {
@@ -64,7 +58,23 @@ impl ProjectDirectory {
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[derive(Debug)]
+enum CliError {
+    Io(std::io::Error, Option<PathBuf>),
+    Serialization(serde_json::Error),
+}
+impl From<std::io::Error> for CliError {
+    fn from(value: std::io::Error) -> Self {
+        CliError::Io(value, None)
+    }
+}
+impl From<serde_json::Error> for CliError {
+    fn from(value: serde_json::Error) -> Self {
+        CliError::Serialization(value)
+    }
+}
+
+fn main() -> Result<(), CliError> {
     let args = Cli::parse();
     env_logger::builder()
         .filter_level(args.verbosity.log_level_filter())
@@ -82,16 +92,18 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut project_directory = ProjectDirectory::new(project_root_abs.clone().into());
     for (link_name, dest_path) in res.module_aliases.iter() {
-        project_directory.add_link(link_name, dest_path)?
+        project_directory.add_link(link_name, dest_path);
     }
     for (link_path, dest_path) in project_directory.get_absolute_links() {
         let link_dir_abs = link_path.parent().unwrap();
         debug!("Ensuring dir {}", link_dir_abs.display());
-        fs::create_dir_all(link_dir_abs)?;
+        fs::create_dir_all(link_dir_abs)
+            .map_err(|err| CliError::Io(err, Some(link_dir_abs.into())))?;
 
         if link_path.exists() || link_path.is_symlink() {
             debug!("Removing file {}", link_path.display());
-            fs::remove_file(link_path.clone())?;
+            fs::remove_file(link_path.clone())
+                .map_err(|err| CliError::Io(err, Some(link_path.clone())))?;
         }
         info!(
             "Linking: {} -> {}",
